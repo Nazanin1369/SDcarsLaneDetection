54c409afbd8049a3e15cd472b6d956671d59a627
nazanin1369 <nazanin1427@gmail.com>
2017-02-17 23:12:39 -0800

    :star: region + color

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ color_thresholds = cs.setColorThresholds(image, color_select)
 # Find the region inside the lines
 region_thresholds = rgn.defineRegionOfInterest(image)
 
+region_color_select = imgUtil.copyImage(color_select)
+region_color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
 # Find where image is both colored right and in the region
 region_select[~color_thresholds & region_thresholds] = [255,0,0]
 
 #Save image
 imgUtil.displayAndSaveImage(color_select, './imgs/color-lane-selection.jpg')
 imgUtil.displayAndSaveImage(region_select, './imgs/region-lane-selection.jpg')
+imgUtil.displayAndSaveImage(region_color_select, './imgs/region-color-lane-selection.jpg')
 
